e4b870f8bd238d66944399cf4a77af9ed16b700e
Divyansh11000 <divyansh.24888@gmail.com>
2020-01-27 00:59:52 +0530

    1.1.2

--- act.js
+++ act.js
@@ -23,3 +23,59 @@ function dowhenresized(){
 dowhenresized();
 
 window.addEventListener("resize", dowhenresized);
+
+
+function dowhenresized2(){
+    var ctext2 = document.getElementById("centered2");
+
+    var win = window.innerWidth;
+    if(win >= 992){
+        ctext2.classList.remove("smaller");
+        ctext2.classList.remove("smallest");
+        ctext2.classList.add("normal");
+    }
+
+    else if (win >= 768){
+        ctext2.classList.remove("normal");
+        ctext2.classList.remove("smallest");
+        ctext2.classList.add("smaller");
+    }
+
+    else if ( win < 768){
+        ctext2.classList.remove("normal");
+        ctext2.classList.remove("smaller");
+        ctext2.classList.add("smallest");
+    }
+}
+dowhenresized2();
+
+window.addEventListener("resize", dowhenresized2);
+
+
+
+
+function dowhenresized3(){
+    var ctext3 = document.getElementById("centered3");
+
+    var win = window.innerWidth;
+    if(win >= 992){
+        ctext3.classList.remove("smaller");
+        ctext3.classList.remove("smallest");
+        ctext3.classList.add("normal");
+    }
+
+    else if (win >= 768){
+        ctext3.classList.remove("normal");
+        ctext3.classList.remove("smallest");
+        ctext3.classList.add("smaller");
+    }
+
+    else if ( win < 768){
+        ctext3.classList.remove("normal");
+        ctext3.classList.remove("smaller");
+        ctext3.classList.add("smallest");
+    }
+}
+dowhenresized3();
+
+window.addEventListener("resize", dowhenresized3);
